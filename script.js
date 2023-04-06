af6b1574f09aa45e9b9977a601eb163b0ab918c0
hp1heloisa <heloisapimentel2202@gmail.com>
2023-04-06 16:24:36 -0300

    versao sem mobile

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let imagens = ['./imagens/bobrossparrot.gif','./imagens/explodyparrot.gif','./imagens/fiestaparrot.gif',
 './imagens/metalparrot.gif','./imagens/revertitparrot.gif', './imagens/tripletsparrot.gif',
 './imagens/unicornparrot.gif']
-let quantidade = prompt('Quantas cartas você quer?')
+let quantidade = prompt('Escolha um número de cartas entre 4 e 14?')
 while (4>quantidade || quantidade>14 || (quantidade%2)!=0){
     quantidade = prompt('Quantas cartas você quer?');
 }
@@ -19,14 +19,64 @@ for (i=0;i<quantidade;i++){
                             <img src="${selecionadas[i]}" class="versocarta face" />
                         </div>`
 }
-
+let cartasviradas = [];
+let nomes = [];
+let jogadas = 0;
+let tamanho = 0;
 function virarcarta(carta){
     let frente = carta.querySelector('.frentecarta');
-    frente.setAttribute("style","transform: rotateY(-180deg);");
     let tras = carta.querySelector('.versocarta');
-    tras.setAttribute("style","transform: rotateY(0deg);");
+    tamanho = cartasviradas.length
+    function desvirar(){
+        let antestras = nomes[tamanho-1].querySelector('.versocarta');
+        let antesfrente = nomes[tamanho-1].querySelector('.frentecarta');
+        antestras.classList.remove('back');
+        antesfrente.classList.remove('front');
+        tras.classList.remove('back');
+        frente.classList.remove('front');
+        nomes.pop();
+        cartasviradas.pop();
+    }
+    let teste = 0;
+    for (i=0;i<tamanho;i++){
+        if (tras.getAttribute("src")==cartasviradas[i]){
+            teste++;
+        }
+    }
+    if (tamanho==0){
+        cartasviradas.push(tras.getAttribute("src"));
+        nomes.push(tras.parentNode);
+        frente.classList.add('front');
+        tras.classList.add('back');
+    }
+    else if (cartasviradas[tamanho-1]==tras.getAttribute("src")){
+        if (teste<2){
+            frente.classList.add('front');
+            tras.classList.add('back');
+            cartasviradas.push(tras.getAttribute("src"));
+            nomes.push(tras.parentNode);
+        }
+    } else if (cartasviradas[tamanho-1]!=tras.getAttribute("src") && (tamanho%2)!=0){
+        frente.classList.add('front');
+        tras.classList.add('back');
+        setTimeout(desvirar,1000);
+    } else{
+        if (teste<2){
+            frente.classList.add('front');
+            tras.classList.add('back');
+            cartasviradas.push(tras.getAttribute("src"));
+            nomes.push(tras.parentNode);
+        }
+    }
+    jogadas++;
+    tamanho = cartasviradas.length;
+    function ganho(){
+        alert(`Você ganhou o jogo em ${jogadas} jogadas!`);
+    }
+    if (tamanho==quantidade){
+        setTimeout(ganho,500);
+    }
 }
-
 function comparador() { 
 	return Math.random() - 0.5; 
 }
